140dc1b1afaf8f535d9db2ca299ba8eebabcfe60
archie.cobbs <archie.cobbs@680dddea-7426-a06b-ac8e-f3e7e60a809d>
2014-02-08 15:37:30 +0000

    Merge scripts.

--- autogen.sh
+++ autogen.sh
@@ -10,8 +10,16 @@
 
 set -e
 
-. ./cleanup.sh
-mkdir -p scripts
+echo "cleaning up"
+rm -rf autom4te*.cache scripts aclocal.m4 configure config.log config.status .deps stamp-h1
+rm -f config.h.in config.h.in~ config.h
+rm -f scripts
+find . \( -name Makefile -o -name Makefile.in \) -print0 | xargs -0 rm -f
+rm -f *.o mtree
+rm -f mtree-?.?.?.tar.gz
+if [ "${1}" = '-C' ]; then
+    exit 0
+fi
 
 ACLOCAL="aclocal"
 AUTOHEADER="autoheader"
@@ -19,6 +27,7 @@ AUTOMAKE="automake"
 AUTOCONF="autoconf"
 
 echo "running aclocal"
+mkdir scripts
 ${ACLOCAL} ${ACLOCAL_ARGS} -I scripts
 
 echo "running autoheader"
@@ -29,4 +38,9 @@ ${AUTOMAKE} --add-missing -c --foreign
 
 echo "running autoconf"
 ${AUTOCONF} -f -i
+
+if [ "${1}" = '-c' ]; then
+    echo "running configure"
+    ./configure
+fi
 
